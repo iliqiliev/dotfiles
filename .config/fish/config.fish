11d67716953856e2af5c5ed3c9340d6b0872396a
Iliya Iliev <iliq0000@proton.me>
2025-08-26 21:32:04 +0300

    Updated bootstrap to override local files by default.
    Also reordered fish conf a bit.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
+# Disabling fish greeting
+set --export fish_greeting
+
 # Setting XDG Base Directories
 # User
 set --export XDG_CACHE_HOME    $HOME/.cache
@@ -29,44 +32,45 @@ set --export WINEPREFIX            $XDG_DATA_HOME/wine
 set --export HISTFILE              $XDG_STATE_HOME/bash/history
 set --export LESSHISTFILE          $XDG_STATE_HOME/less/history
 set --export PYTHON_HISTORY        $XDG_STATE_HOME/python_history
-set --export fish_greeting
 
-alias adb "HOME=$XDG_DATA_HOME/android command adb"
-alias wget "wget --hsts-file='$XDG_STATE_HOME/wget-hsts'"
+if grep --quiet --no-messages amdgpu /proc/modules
+    set --export RUSTICL_ENABLE radeonsi # enables rusticl OpenCL driver
+    set --export VDPAU_DRIVER radeonsi
+    set --export LIBVA_DRIVER_NAME radeonsi
+end
 
-if command --quiet eza
-    alias ls="eza"
-    alias tree="eza --all --tree"
+if command --quiet adb
+    alias adb "HOME=$XDG_DATA_HOME/android command adb"
+end
+
+if command --quiet wget
+    alias wget "wget --hsts-file='$XDG_STATE_HOME/wget-hsts'"
 end
 
 if command --quiet bat
     alias cat="bat --paging=never --style=plain"
 end
 
+if command --quiet eza
+    alias ls="eza"
+    alias tree="eza --all --tree"
+end
+
 if command --quiet micro
     set --export EDITOR micro
 end
 
-if grep --quiet --no-messages amdgpu /proc/modules
-    set --export RUSTICL_ENABLE radeonsi # enables rusticl opnencl driver
-    set --export VDPAU_DRIVER radeonsi
-    set --export LIBVA_DRIVER_NAME radeonsi
-end
-
-# [zoxide](https://github.com/ajeetdsouza/zoxide)
 if command --quiet zoxide  
     zoxide init fish | source
 end
 
 # Abbreviations
 abbr config_fish "$EDITOR $HOME/.config/fish/config.fish"
-abbr sc "systemctl"
-abbr ssc "sudo systemctl"
+abbr sc  "systemctl"
 abbr scu "systemctl --user"
+abbr ssc "sudo systemctl"
 abbr fp "flatpak"
 abbr ff "fastfetch"
 
 # Adding directories to $PATH
 fish_add_path $HOME/.local/bin /var/lib/flatpak/exports/bin/
-
-
