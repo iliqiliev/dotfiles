47f892252deb8b75dc70cb9ac94d3a48a9f21015
Iliya Iliev <iliq0000@proton.me>
2026-02-22 17:27:56 +0200

    Rename .data to data

--- .chezmoiscripts/linux/arch/run_onchange_sched-ext.bash
+++ .chezmoiscripts/linux/arch/run_onchange_sched-ext.bash
@@ -3,5 +3,5 @@ if systemctl list-unit-files scx_loader.service &>/dev/null; then
 fi
 
 $CHEZ_SUDO pacman -S --needed --noconfirm scx-scheds scx-tools
-$CHEZ_SUDO cp "$CHEZMOI_SOURCE_DIR"/.data/scx_loader.toml /etc/scx_loader.toml
+$CHEZ_SUDO cp "$CHEZMOI_SOURCE_DIR"/data/scx_loader.toml /etc/scx_loader.toml
 $CHEZ_SUDO systemctl enable --now scx_loader.service
