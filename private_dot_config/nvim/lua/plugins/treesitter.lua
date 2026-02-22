38cf934f86427f955af37b0111b45a83bb494bd8
Iliya Iliev <iliq0000@proton.me>
2026-02-22 14:39:21 +0200

    Format lua plugins

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-	branch = 'master',
-	lazy = false,
-	build = ":TSUpdate",
-	opts = {
-	    highlight = {
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
+        opts = {
+            highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
-  	    },
-	    injections = { enable = true },
-	}
+            },
+            injections = { enable = true },
+        }
     }
 }
