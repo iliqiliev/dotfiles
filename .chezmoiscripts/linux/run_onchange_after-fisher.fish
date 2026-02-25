6264c10c9ce72295828212e05521facf91954cdb
Iliya Iliev <iliq0000@proton.me>
2026-02-25 22:19:18 +0200

    Update run_onchange_after-fisher.fish

--- .chezmoiscripts/linux/run_onchange_after-fisher.fish
+++ .chezmoiscripts/linux/run_onchange_after-fisher.fish
@@ -1,13 +1,14 @@
-echo -e $BLUE"Setting up fisher..."$NORMAL
-
 if not type --query "fisher"
+    echo -e $BLUE"Installing fisher..."$NORMAL
+
     if not curl -sL git.io/fisher | source
         echo -e $RED"Fisher install failed."$NORMAL >&2
         exit 0
     end
+
+    echo -e $GREEN"Fisher installed successfully."$NORMAL
 end
 
-echo -e $BLUE"Installing fish plugins..."$NORMAL
+echo -e $BLUE"Installing fisher plugins..."$NORMAL
 fisher update > /dev/null
-
-echo -e $GREEN"Installed $(count (fisher list)) fish plugins."$NORMAL
+and echo -e $GREEN"Installed $(count (fisher list)) fish plugins."$NORMAL
