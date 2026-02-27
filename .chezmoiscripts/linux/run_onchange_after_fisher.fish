989557a03f34f199b8f11af9b5ee19046d7bbba4
Iliya Iliev <iliq0000@proton.me>
2026-02-27 22:11:48 +0200

    Add eval for multi word cmd variable in fish

--- .chezmoiscripts/linux/run_onchange_after_fisher.fish
+++ .chezmoiscripts/linux/run_onchange_after_fisher.fish
@@ -1,7 +1,7 @@
 if not type --query "fisher"
     echo -e $BLUE"Installing fisher..."$NORMAL
 
-    if not $CHEZ_FETCH git.io/fisher | source
+    if not eval $CHEZ_FETCH git.io/fisher | source
         echo -e $RED"Fisher install failed."$NORMAL >&2
         exit 0
     end
