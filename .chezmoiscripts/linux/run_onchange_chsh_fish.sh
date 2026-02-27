81ec3fa359f95715d6a598c59fff545eab001c61
Iliya Iliev <iliq0000@proton.me>
2026-02-27 22:41:34 +0200

    Allow changing shell with unset $USER

--- .chezmoiscripts/linux/run_onchange_chsh_fish.sh
+++ .chezmoiscripts/linux/run_onchange_chsh_fish.sh
@@ -26,5 +26,6 @@ if [ ! -f /usr/bin/fish ]; then
     exit 0
 fi
 
-$CHEZ_SUDO chsh --shell /usr/bin/fish "$USER"
+#shellcheck disable=2086
+$CHEZ_SUDO chsh --shell /usr/bin/fish $USER
 echo -e "${GREEN}Shell changed to fish sucessfully.${NORMAL}"
