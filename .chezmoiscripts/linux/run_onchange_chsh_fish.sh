140fd137bce5a3ad9a3d27c290d8ef1d46fafc63
Iliya Iliev <iliq0000@proton.me>
2026-03-11 09:49:58 +0200

    msg: tweaks

--- .chezmoiscripts/linux/run_onchange_chsh_fish.sh
+++ .chezmoiscripts/linux/run_onchange_chsh_fish.sh
@@ -36,7 +36,7 @@ if [ -z "$FISH_SHELL" ]; then
 fi
 
 if ! $FISH_SHELL --version | grep --quiet "[4-9].[0-9].[0-9]"; then
-    echo -e "${RED}$($FISH_SHELL --version) is older than v4.0.0, aborting.${NORMAL}"
+    echo -e "${RED}$($FISH_SHELL --version) is older than version 4.0.0, aborting.${NORMAL}"
     exit 0
 fi
 
