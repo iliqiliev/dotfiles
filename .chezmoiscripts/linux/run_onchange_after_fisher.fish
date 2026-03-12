b6c7f9421d336113ee3d89e27f41e4c4e611ba26
Iliya Iliev <iliq0000@proton.me>
2026-03-12 17:44:16 +0200

    fisher: check fish version
    https://stackoverflow.com/questions/19211855/fish-equivalent-of-bash-command-notation

--- .chezmoiscripts/linux/run_onchange_after_fisher.fish
+++ .chezmoiscripts/linux/run_onchange_after_fisher.fish
@@ -1,5 +1,12 @@
+echo -e $BLUE"Installing fisher plugins..."$NORMAL
+
+if not fish_version_check 3 4 0
+    echo -e $RED"Fish version is older than 3.4, aborting."$NORMAL >&2
+    exit 0
+end
+
 if not type --query "fisher"
-    echo -e $BLUE"Installing fisher..."$NORMAL
+    echo -e $BLUE"Fisher is not installed. Installing it first..."$NORMAL
 
     if not eval $CHEZ_FETCH https://git.io/fisher | source
         echo -e $RED"Fisher install failed."$NORMAL >&2
@@ -9,6 +16,5 @@ if not type --query "fisher"
     echo -e $GREEN"Fisher installed successfully."$NORMAL
 end
 
-echo -e $BLUE"Installing fisher plugins..."$NORMAL
 fisher update > /dev/null
 and echo -e $GREEN"Installed $(count (fisher list)) fish plugins."$NORMAL
