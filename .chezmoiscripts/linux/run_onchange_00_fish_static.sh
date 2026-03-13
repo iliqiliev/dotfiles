cbe71c077973b0b316b58aaa1f9673586f62bee6
Iliya Iliev <iliq0000@proton.me>
2026-03-13 18:51:49 +0200

    fish: add https to static url

--- .chezmoiscripts/linux/run_onchange_00_fish_static.sh
+++ .chezmoiscripts/linux/run_onchange_00_fish_static.sh
@@ -12,7 +12,7 @@ fi
 
 FISH_VERSION=4.5.0
 ARCH=$(uname --machine)
-BASE_URL="github.com/fish-shell/fish-shell/releases/download"
+BASE_URL="https://github.com/fish-shell/fish-shell/releases/download"
 FISH_URL="${BASE_URL}/${FISH_VERSION}/fish-${FISH_VERSION}-linux-${ARCH}.tar.xz"
 FISH_LOCAL_PATH="$HOME/.local/bin/fish"
 
