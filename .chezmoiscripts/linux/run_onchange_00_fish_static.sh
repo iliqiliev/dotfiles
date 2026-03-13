1607a6e317cfe4fae6392eac0aa62cc60fc0b76c
Iliya Iliev <iliq0000@proton.me>
2026-03-13 18:19:56 +0200

    fish: add static install if system version is too old

---
+++ .chezmoiscripts/linux/run_onchange_00_fish_static.sh
@@ -0,0 +1,18 @@
+if fish_version_check 4 0 0 2>/dev/null; then
+    exit 0
+else
+    FISH_VERSION_STATUS=$?
+fi
+
+if [ $FISH_VERSION_STATUS -eq 127 ]; then
+    echo -e "${BLUE}Fish shell is not installed. Installing static build...${NORMAL}"
+else
+    echo -e "${BLUE}Fish shell version is too old. Installing static build...${NORMAL}"
+fi
+
+FISH_VERSION=4.5.0
+ARCH=$(uname --machine)
+BASE_URL="github.com/fish-shell/fish-shell/releases/"
+FISH_URL="${BASE_URL}/${FISH_VERSION}/fish-${FISH_VERSION}-linux-${ARCH}.tar.xz"
+
+$CHEZ_FETCH "$FISH_URL" | tar xfJ >"$HOME"/.local/bin/fish
