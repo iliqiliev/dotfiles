5aa11816c207b0c490fb29c2a44d87afd6e0ef39
Iliya Iliev <iliq0000@proton.me>
2026-03-13 18:54:18 +0200

    fish: fix install location

--- .chezmoiscripts/linux/run_onchange_00_fish_static.sh
+++ .chezmoiscripts/linux/run_onchange_00_fish_static.sh
@@ -14,11 +14,11 @@ FISH_VERSION=4.5.0
 ARCH=$(uname --machine)
 BASE_URL="https://github.com/fish-shell/fish-shell/releases/download"
 FISH_URL="${BASE_URL}/${FISH_VERSION}/fish-${FISH_VERSION}-linux-${ARCH}.tar.xz"
-FISH_LOCAL_PATH="$HOME/.local/bin/fish"
+LOCAL_BIN="$HOME/.local/bin"
 
 echo -e "${BLUE}Downloading from '${FISH_URL}' ...${NORMAL}"
 
-$CHEZ_FETCH "$FISH_URL" | tar --extract --xz >"$FISH_LOCAL_PATH"
-chmod +x "$FISH_LOCAL_PATH"
+$CHEZ_FETCH "$FISH_URL" | tar --extract --xz --directory "$LOCAL_BIN"
+chmod +x "$LOCAL_BIN"
 
-echo -e "${GREEN}Fish static installed to '${FISH_LOCAL_PATH}' successfully.${NORMAL}"
+echo -e "${GREEN}Fish static installed to '${LOCAL_BIN}' successfully.${NORMAL}"
