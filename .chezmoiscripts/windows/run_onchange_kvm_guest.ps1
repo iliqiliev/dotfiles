c642f6130d01d3a1f11c017012c4a27607451788
Iliya Iliev <iliq0000@proton.me>
2026-03-20 15:04:56 +0200

    scoop: remove iliya prefix from virtio-guest-tools

--- .chezmoiscripts/windows/run_onchange_kvm_guest.ps1
+++ .chezmoiscripts/windows/run_onchange_kvm_guest.ps1
@@ -10,5 +10,5 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
     + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 Write-Host "System detected as a KVM guest. Installing guest tools." -ForegroundColor Blue
-gsudo scoop install --no-update-scoop iliya/virtio-guest-tools; if (!$?) { exit 1 }
+gsudo scoop install --no-update-scoop virtio-guest-tools; if (!$?) { exit 1 }
 Write-Host "KVM guest tools successfully installed." -ForegroundColor Green
