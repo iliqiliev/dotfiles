768524dd8e81fc125dd71f8102fdb17b7b9bbd73
Iliya Iliev <iliq0000@proton.me>
2026-03-21 19:33:39 +0200

    powershell: don't fill completions if multiple available

--- AppData/Roaming/WindowsPowerShell/profile.ps1
+++ AppData/Roaming/WindowsPowerShell/profile.ps1
@@ -1,4 +1,5 @@
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 
 if (Get-Command starship -ErrorAction SilentlyContinue) {
