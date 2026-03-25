66e3ed4a05af2fef0a53f0a74a95f8f78b08a81c
Iliya Iliev <iliq0000@proton.me>
2026-03-25 18:04:15 +0200

    Revert "windows: use Update-Path function in scripts"
    This reverts commit 82afd959e14305f64c96ccc649a0f462d103ca57.

--- .chezmoiscripts/windows/run_onchange_clink.ps1
+++ .chezmoiscripts/windows/run_onchange_clink.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-Update-Path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
+    + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 Write-Host "Setting up clink autorun." -ForegroundColor Blue
 clink autorun install; if (!$?) { exit 1 }
--- .chezmoiscripts/windows/run_onchange_kvm_guest.ps1
+++ .chezmoiscripts/windows/run_onchange_kvm_guest.ps1
@@ -6,7 +6,8 @@ if (!($system_info.Manufacturer -match "QEMU")) {
     exit 0
 }
 
-Update-Path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
+    + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 Write-Host "System detected as a KVM guest. Installing guest tools." -ForegroundColor Blue
 gsudo scoop install --no-update-scoop virtio-guest-tools; if (!$?) { exit 1 }
