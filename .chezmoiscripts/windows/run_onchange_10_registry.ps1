226abe5c9da1657f9f836e2d879ceac22bd668f8
Iliya Iliev <iliq0000@proton.me>
2026-03-25 10:24:04 +0200

    registry: remove positional args flags; make more readable

--- .chezmoiscripts/windows/run_onchange_10_registry.ps1
+++ .chezmoiscripts/windows/run_onchange_10_registry.ps1
@@ -3,36 +3,42 @@ $ErrorActionPreference = "Stop"
 Write-Host "Setting registry settings..." -ForegroundColor Blue
 
 $REG_PATH = "HKCU:\Control Panel\Mouse"
-Set-ItemProperty -Path $REG_PATH -Name "MouseSpeed" -Value "0"
-Set-ItemProperty -Path $REG_PATH -Name "MouseThreshold1" -Value "0"
-Set-ItemProperty -Path $REG_PATH -Name "MouseThreshold2" -Value "0"
+Set-ItemProperty $REG_PATH "MouseSpeed"      "0"
+Set-ItemProperty $REG_PATH "MouseThreshold1" "0"
+Set-ItemProperty $REG_PATH "MouseThreshold2" "0"
 
 $REG_PATH = "HKCU:\Environment"
-Set-ItemProperty -Path $REG_PATH -Name "XDG_CACHE_HOME" -Value "%USERPROFILE%\.cache" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "XDG_CONFIG_HOME" -Value "%USERPROFILE%\.config" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "XDG_STATE_HOME" -Value "%USERPROFILE%\.local\state" -Type ExpandString
-
-Set-ItemProperty -Path $REG_PATH -Name "BAT_CONFIG_PATH" -Value "%USERPROFILE%\.config\bat\config" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "HISTFILE" -Value "%USERPROFILE%\.local\state\bash\history" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "PYTHONPYCACHEPREFIX" -Value "%USERPROFILE%\.cache\python" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "PYTHON_HISTORY" -Value "%USERPROFILE%\.local\state\python_history" -Type ExpandString
-Set-ItemProperty -Path $REG_PATH -Name "CLINK_PROFILE" -Value "%APPDATA%\clink"
-
-Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value "Restore the classic context menu in Windows 11."
+Set-ItemProperty $REG_PATH "XDG_CACHE_HOME"      "%USERPROFILE%\.cache"                      -Type ExpandString
+Set-ItemProperty $REG_PATH "XDG_CONFIG_HOME"     "%USERPROFILE%\.config"                     -Type ExpandString
+Set-ItemProperty $REG_PATH "XDG_STATE_HOME"      "%USERPROFILE%\.local\state"                -Type ExpandString
+Set-ItemProperty $REG_PATH "BAT_CONFIG_PATH"     "%USERPROFILE%\.config\bat\config"          -Type ExpandString
+Set-ItemProperty $REG_PATH "HISTFILE"            "%USERPROFILE%\.local\state\bash\history"   -Type ExpandString
+Set-ItemProperty $REG_PATH "PYTHONPYCACHEPREFIX" "%USERPROFILE%\.cache\python"               -Type ExpandString
+Set-ItemProperty $REG_PATH "PYTHON_HISTORY"      "%USERPROFILE%\.local\state\python_history" -Type ExpandString
+Set-ItemProperty $REG_PATH "CLINK_PROFILE"       "%APPDATA%\clink"
 
 $REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-Set-ItemProperty -Path $REG_PATH -Name "DisabledHotkeys" -Value "V"
-Set-ItemProperty -Path $REG_PATH -Name "Hidden" -Value 1
-Set-ItemProperty -Path $REG_PATH -Name "HideFileExt" -Value 0
-Set-ItemProperty -Path $REG_PATH -Name "TaskbarAl" -Value 0
+Set-ItemProperty $REG_PATH "DisabledHotkeys" "V"
+Set-ItemProperty $REG_PATH "Hidden"           1
+Set-ItemProperty $REG_PATH "HideFileExt"      0
+Set-ItemProperty $REG_PATH "TaskbarAl"        0
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" `
+                 -Name "(Default)" `
+                 -Value "Restore the classic context menu in Windows 11."
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Personal" -Value "%APPDATA%" -Type ExpandString
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
+                 -Name "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" `
+                 -Value 1
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" `
+                 -Name "Personal" `
+                 -Value "%APPDATA%" `
+                 -Type ExpandString
 
 Write-Host "Registry settings imported successfully." -ForegroundColor Green
 
-Write-Host "Restarting explorer.exe" -ForegroundColor Blue
-taskkill -f -im explorer.exe; if (!$?) { exit 1 }
-Start-Process explorer.exe
-Write-Host "explorer.exe restarted successfully." -ForegroundColor Green
+# Write-Host "Restarting explorer.exe" -ForegroundColor Blue
+# taskkill -f -im explorer.exe; if (!$?) { exit 1 }
+# Start-Process explorer.exe
+# Write-Host "explorer.exe restarted successfully." -ForegroundColor Green
