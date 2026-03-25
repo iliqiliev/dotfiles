c9bad6733bb0ef000485f77969cfdb7e06b42ae1
Iliya Iliev <iliq0000@proton.me>
2026-03-25 17:50:19 +0200

    powershell: add path update function to profile

--- AppData/Roaming/WindowsPowerShell/profile.ps1
+++ AppData/Roaming/WindowsPowerShell/profile.ps1
@@ -1,6 +1,10 @@
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
+function Update-Path {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
+        + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     Invoke-Expression (&starship init powershell)
