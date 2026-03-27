fa8a87aec3529b004b5e24c69ecf0d36dd5cc437
Iliya Iliev <iliq0000@proton.me>
2026-03-27 16:13:17 +0200

    registry: add TEALDEER_CONFIG_DIR variable

--- .chezmoiscripts/windows/run_onchange_10_registry.ps1
+++ .chezmoiscripts/windows/run_onchange_10_registry.ps1
@@ -10,11 +10,12 @@ Set-ItemProperty $REG_PATH "MouseThreshold2" "0"
 $REG_PATH = "HKCU:\Environment" # Set environment variables.
 Set-ItemProperty $REG_PATH "CLINK_PROFILE"       "%APPDATA%\clink"                           -Type ExpandString
 Set-ItemProperty $REG_PATH "XDG_CACHE_HOME"      "%USERPROFILE%\.cache"                      -Type ExpandString
-Set-ItemProperty $REG_PATH "XDG_CONFIG_HOME"     "%USERPROFILE%\.config"                     -Type ExpandString
-Set-ItemProperty $REG_PATH "XDG_STATE_HOME"      "%USERPROFILE%\.local\state"                -Type ExpandString
-Set-ItemProperty $REG_PATH "BAT_CONFIG_PATH"     "%USERPROFILE%\.config\bat\config"          -Type ExpandString
-Set-ItemProperty $REG_PATH "HISTFILE"            "%USERPROFILE%\.local\state\bash\history"   -Type ExpandString
 Set-ItemProperty $REG_PATH "PYTHONPYCACHEPREFIX" "%USERPROFILE%\.cache\python"               -Type ExpandString
+Set-ItemProperty $REG_PATH "XDG_CONFIG_HOME"     "%USERPROFILE%\.config"                     -Type ExpandString
+Set-ItemProperty $REG_PATH "BAT_CONFIG_PATH"     "%USERPROFILE%\.config\bat\config"          -Type ExpandString
+Set-ItemProperty $REG_PATH "TEALDEER_CONFIG_DIR" "%USERPROFILE%\.config\tealdeer"            -Type ExpandString
+Set-ItemProperty $REG_PATH "XDG_STATE_HOME"      "%USERPROFILE%\.local\state"                -Type ExpandString
+Set-ItemProperty $REG_PATH "HISTFILE"            "%USERPROFILE%\.local\state\bash\history"   -Type ExpandString
 Set-ItemProperty $REG_PATH "PYTHON_HISTORY"      "%USERPROFILE%\.local\state\python_history" -Type ExpandString
 
 $REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
