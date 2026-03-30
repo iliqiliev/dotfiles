15b39044826ee9a44137f004743d73b3c47d0bda
Iliya Iliev <iliq0000@proton.me>
2026-03-30 18:08:17 +0300

    bash: make chezmoi specific functions posix compatible
    for alpine

--- .chezmoi/chezmoi.bash
+++ .chezmoi/chezmoi.bash
@@ -1,21 +1,23 @@
+#!/usr/bin/env sh
+
 NORMAL='\033[0m'
 
-function print_red() {
+print_red() {
     RED='\033[31m'
     printf "${RED}%b\n${NORMAL}" "$*"
 }
 
-function print_green() {
+print_green() {
     GREEN='\033[32m'
     printf "${GREEN}%b\n${NORMAL}" "$*"
 }
 
-function print_yellow() {
+print_yellow() {
     YELLOW='\033[33m'
     printf "${YELLOW}%b\n${NORMAL}" "$*"
 }
 
-function print_blue() {
+print_blue() {
     BLUE='\033[34m'
     printf "${BLUE}%b\n${NORMAL}" "$*"
 }
