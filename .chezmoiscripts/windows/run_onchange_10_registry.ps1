01aaac2401a8722e09565a3c29b4fe5a5b4404d7
Iliya Iliev <iliq0000@proton.me>
2026-04-01 16:13:37 +0300

    registry: hide search box and widgets from taskbar

--- .chezmoiscripts/windows/run_onchange_10_registry.ps1
+++ .chezmoiscripts/windows/run_onchange_10_registry.ps1
@@ -25,10 +25,11 @@ Set-ItemProperty $REG_PATH "MouseThreshold1" "0"
 Set-ItemProperty $REG_PATH "MouseThreshold2" "0"
 
 $REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-Set-ItemProperty $REG_PATH "DisabledHotkeys" "V"
-Set-ItemProperty $REG_PATH "Hidden"           1
-Set-ItemProperty $REG_PATH "HideFileExt"      0
-Set-ItemProperty $REG_PATH "TaskbarAl"        0
+Set-ItemProperty $REG_PATH "DisabledHotkeys" "V" # Free Super+V shortcut for external clipboard manager.
+Set-ItemProperty $REG_PATH "Hidden"           1  # Show hidden files and folders in explorer.
+Set-ItemProperty $REG_PATH "HideFileExt"      0  # Show file extensions in explorer.
+Set-ItemProperty $REG_PATH "TaskbarAl"        0  # Align taskbar to the left.
+Set-ItemProperty $REG_PATH "TaskbarDa"        0  # Hide widgets from the taskbar.
 
 Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" `
                  -Name "(Default)" `
@@ -42,6 +43,10 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
                  -Name "Personal" `
                  -Value "%APPDATA%" `
                  -Type ExpandString # Move PowerShell's profile directory.
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" `
+                 -Name "SearchboxTaskbarMode" `
+                 -Value 0 # Hide the taskbar search box.
 
 Write-Host "Registry settings imported successfully." -ForegroundColor Green
 
