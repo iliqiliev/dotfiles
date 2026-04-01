287606efcd628903553cb14c73b1ba699054e35e
Iliya Iliev <iliq0000@proton.me>
2026-04-01 16:28:01 +0300

    registry: improve spacing

--- .chezmoiscripts/windows/run_onchange_10_registry.ps1
+++ .chezmoiscripts/windows/run_onchange_10_registry.ps1
@@ -24,15 +24,22 @@ Set-ItemProperty $REG_PATH "MouseSpeed"      "0"
 Set-ItemProperty $REG_PATH "MouseThreshold1" "0"
 Set-ItemProperty $REG_PATH "MouseThreshold2" "0"
 
-$REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-Set-ItemProperty $REG_PATH "DisabledHotkeys" "V" # Free Super+V shortcut for external clipboard manager.
-Set-ItemProperty $REG_PATH "Hidden"           1  # Show hidden files and folders in explorer.
-Set-ItemProperty $REG_PATH "HideFileExt"      0  # Show file extensions in explorer.
-Set-ItemProperty $REG_PATH "TaskbarAl"        0  # Align taskbar to the left.
-Set-ItemProperty $REG_PATH "TaskbarDa"        0  # Hide widgets from the taskbar.
 
-# Restore the classic context menu in Windows 11.
+$REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+# Free Super+V shortcut for external clipboard manager.
+Set-ItemProperty $REG_PATH "DisabledHotkeys" "V"
+# Show hidden files and folders in explorer.
+Set-ItemProperty $REG_PATH "Hidden"           1
+# Show file extensions in explorer.
+Set-ItemProperty $REG_PATH "HideFileExt"      0
+# Align taskbar to the left.
+Set-ItemProperty $REG_PATH "TaskbarAl"        0
+# Hide widgets from the taskbar.
+Set-ItemProperty $REG_PATH "TaskbarDa"        0 -Error SilentlyContinue
+
+
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
+         -Force # Restore the classic context menu in Windows 11.
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
                  -Name "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" `
@@ -46,6 +53,7 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" `
                  -Name "SearchboxTaskbarMode" `
                  -Value 0 # Hide the taskbar search box.
+
 
 Write-Host "Registry settings imported successfully." -ForegroundColor Green
 
