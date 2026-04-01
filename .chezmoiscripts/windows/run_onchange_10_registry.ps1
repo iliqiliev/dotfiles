b8b1b66341095e10aa4c01daba5bca0070d3ce63
Iliya Iliev <iliq0000@proton.me>
2026-04-01 16:48:48 +0300

    registry: add comment about ucpd

--- .chezmoiscripts/windows/run_onchange_10_registry.ps1
+++ .chezmoiscripts/windows/run_onchange_10_registry.ps1
@@ -5,37 +5,37 @@ Write-Host "Setting registry settings..." -ForegroundColor Blue
 
 $REG_PATH = "HKCU:\Environment" # Set environment variables.
 
-Set-ItemProperty $REG_PATH "CLINK_PROFILE"       "%APPDATA%\clink"                           -Type ExpandString
+Set-ItemProperty $REG_PATH CLINK_PROFILE       "%APPDATA%\clink"                           -Type ExpandString
 
-Set-ItemProperty $REG_PATH "XDG_CACHE_HOME"      "%USERPROFILE%\.cache"                      -Type ExpandString
-Set-ItemProperty $REG_PATH "PYTHONPYCACHEPREFIX" "%USERPROFILE%\.cache\python"               -Type ExpandString
+Set-ItemProperty $REG_PATH XDG_CACHE_HOME      "%USERPROFILE%\.cache"                      -Type ExpandString
+Set-ItemProperty $REG_PATH PYTHONPYCACHEPREFIX "%USERPROFILE%\.cache\python"               -Type ExpandString
 
-Set-ItemProperty $REG_PATH "XDG_CONFIG_HOME"     "%USERPROFILE%\.config"                     -Type ExpandString
-Set-ItemProperty $REG_PATH "BAT_CONFIG_PATH"     "%USERPROFILE%\.config\bat\config"          -Type ExpandString
-Set-ItemProperty $REG_PATH "TEALDEER_CONFIG_DIR" "%USERPROFILE%\.config\tealdeer"            -Type ExpandString
+Set-ItemProperty $REG_PATH XDG_CONFIG_HOME     "%USERPROFILE%\.config"                     -Type ExpandString
+Set-ItemProperty $REG_PATH BAT_CONFIG_PATH     "%USERPROFILE%\.config\bat\config"          -Type ExpandString
+Set-ItemProperty $REG_PATH TEALDEER_CONFIG_DIR "%USERPROFILE%\.config\tealdeer"            -Type ExpandString
 
-Set-ItemProperty $REG_PATH "XDG_STATE_HOME"      "%USERPROFILE%\.local\state"                -Type ExpandString
-Set-ItemProperty $REG_PATH "HISTFILE"            "%USERPROFILE%\.local\state\bash\history"   -Type ExpandString
-Set-ItemProperty $REG_PATH "PYTHON_HISTORY"      "%USERPROFILE%\.local\state\python_history" -Type ExpandString
+Set-ItemProperty $REG_PATH XDG_STATE_HOME      "%USERPROFILE%\.local\state"                -Type ExpandString
+Set-ItemProperty $REG_PATH HISTFILE            "%USERPROFILE%\.local\state\bash\history"   -Type ExpandString
+Set-ItemProperty $REG_PATH PYTHON_HISTORY      "%USERPROFILE%\.local\state\python_history" -Type ExpandString
 
 
 $REG_PATH = "HKCU:\Control Panel\Mouse" # Disable mouse acceleration.
-Set-ItemProperty $REG_PATH "MouseSpeed"      "0"
-Set-ItemProperty $REG_PATH "MouseThreshold1" "0"
-Set-ItemProperty $REG_PATH "MouseThreshold2" "0"
+Set-ItemProperty $REG_PATH MouseSpeed      "0"
+Set-ItemProperty $REG_PATH MouseThreshold1 "0"
+Set-ItemProperty $REG_PATH MouseThreshold2 "0"
 
 
 $REG_PATH = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 # Free Super+V shortcut for external clipboard manager.
-Set-ItemProperty $REG_PATH "DisabledHotkeys" "V"
+Set-ItemProperty $REG_PATH DisabledHotkeys "V"
 # Show hidden files and folders in explorer.
-Set-ItemProperty $REG_PATH "Hidden"           1
+Set-ItemProperty $REG_PATH Hidden           1
 # Show file extensions in explorer.
-Set-ItemProperty $REG_PATH "HideFileExt"      0
+Set-ItemProperty $REG_PATH HideFileExt      0
 # Align taskbar to the left.
-Set-ItemProperty $REG_PATH "TaskbarAl"        0
-# Hide widgets from the taskbar.
-Set-ItemProperty $REG_PATH "TaskbarDa"        0 -ErrorAction SilentlyContinue
+Set-ItemProperty $REG_PATH TaskbarAl        0
+# Hide widgets from the taskbar. Key protected by UCPD: https://kolbi.cz/blog/2024/04/03/userchoice-protection-driver-ucpd-sys
+Set-ItemProperty $REG_PATH TaskbarDa        0 -ErrorAction SilentlyContinue
 
 
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" `
