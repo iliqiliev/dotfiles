3dc1be8683a48e568175c17956228b9e1364994b
Iliya Iliev <iliq0000@proton.me>
2026-04-04 00:11:00 +0300

    chezmoi: add .local/bin to path in powershell profile wrapper

--- .chezmoi/chezmoi.ps1
+++ .chezmoi/chezmoi.ps1
@@ -1,4 +1,10 @@
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
     + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
+$LOCAL_BIN = "$env:PATH\.local\bin"
+
+if ($env:PATH -notlike "*$LOCAL_BIN*") {
+    $env:PATH += ";$LOCAL_BIN"
+}
+
 . $args[0]
