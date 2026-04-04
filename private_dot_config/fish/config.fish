c9bc35ba773e00b9636f60283eca775520ce42cb
Iliya Iliev <iliq0000@proton.me>
2026-04-04 23:44:05 +0300

    fish: exit config if not interactive

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,5 +1,9 @@
 # conf.d is loaded first
 
+if not status is-interactive
+    exit
+end
+
 if not set --query FISH_LOCAL_UPGRADE; and test -f ~/.local/bin/fish
     set --export FISH_LOCAL_UPGRADE 1
     exec ~/.local/bin/fish
