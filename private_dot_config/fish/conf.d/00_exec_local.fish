9705142e0d2b52df944df09cdd6989988a318c36
Iliya Iliev <iliq0000@proton.me>
2026-04-04 23:15:21 +0300

    fish: simplify exec_local

--- private_dot_config/fish/conf.d/00_exec_local.fish
+++ private_dot_config/fish/conf.d/00_exec_local.fish
@@ -6,9 +6,7 @@ if set --query FISH_EXEC_LOCAL
     exit # break infinite recursion
 end
 
-set FISH_LOCAL ~/.local/bin/fish
-set _fish_pid %self
-set fish_path (readlink /proc/$_fish_pid/exe)
+set fish_path (readlink /proc/(echo %self)/exe)
 
 if ~/.local/bin/fish_version 4 0 0 $fish_path
     exit # current fish version is good enough
