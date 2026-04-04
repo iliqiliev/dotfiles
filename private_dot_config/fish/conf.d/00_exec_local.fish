53318bc5b340264c7efe56dc6cc4042e7f446e79
Iliya Iliev <iliq0000@proton.me>
2026-04-04 23:01:57 +0300

    fish: improve exec_local

--- private_dot_config/fish/conf.d/00_exec_local.fish
+++ private_dot_config/fish/conf.d/00_exec_local.fish
@@ -2,16 +2,16 @@
 # at ~/.local/bin/fish, exec it as to not break the system fish shell
 # with the syntax of the config files made for the newer fish versions
 
+if set --query FISH_EXEC_LOCAL
+    exit # break infinite recursion
+end
+
 set FISH_LOCAL ~/.local/bin/fish
 set _fish_pid %self
 set fish_path (readlink /proc/$_fish_pid/exe)
 
 if ~/.local/bin/fish_version 4 0 0 $fish_path
     exit # current fish version is good enough
-end
-
-if set --query FISH_EXEC_LOCAL
-    exit # break infinite recursion
 end
 
 if not test -x $FISH_LOCAL
