45edeced2931cb04e729abc5d44e3d40b80e2945
Iliya Iliev <iliq0000@proton.me>
2026-04-06 10:02:23 +0300

    scoop: impove script

--- .chezmoiscripts/windows/run_once_20_scoop.ps1
+++ .chezmoiscripts/windows/run_once_20_scoop.ps1
@@ -1,17 +1,37 @@
 $ErrorActionPreference = "Stop"
 
 
+$BUCKETS = @(
+    "extras"
+    "sysinternals"
+    "nerd-fonts"
+    "iliya https://github.com/iliqiliev/iliya-bucket"
+)
+
 if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
 	Write-Host "Scoop not found. Installing it..." -ForegroundColor Blue
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
 Write-Host "Setting up scoop buckets." -ForegroundColor Blue
-scoop install --no-update-scoop git; if (!$?) { exit 1 }
-scoop bucket add extras
-scoop bucket add sysinternals
-scoop bucket add nerd-fonts
-scoop bucket add iliya https://github.com/iliqiliev/iliya-bucket
+
+if (!(Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Host "Git not found. Installing it..." -ForegroundColor Blue
+    scoop install --no-update-scoop git
+    Write-Host "Git installed successfully." -ForegroundColor Green
+}
+
+foreach ($bucket in $BUCKETS) {
+    $name, $url = $bucket.Split(" ", 2)
+
+    if (Test-Path "$env:USERPROFILE\scoop\buckets\$name" -PathType Container) {
+        Write-Host "The $name bucket is added already." -ForegroundColor Green
+        continue
+    }
+
+    scoop bucket add $name $url
+}
+
 Write-Host "Scoop buckets added successfully." -ForegroundColor Green
 
 Write-Host "Installing scoop packages..." -ForegroundColor Blue
