a2631fa0de144434c693a2f9ae8541f730e22b7f
Iliya Iliev <iliq0000@proton.me>
2026-04-08 13:00:02 +0300

    fish: update non-interactive sessions

--- private_dot_config/fish/conf.d/00_exec_local.fish
+++ private_dot_config/fish/conf.d/00_exec_local.fish
@@ -2,20 +2,19 @@
 # at ~/.local/bin/fish, exec it as to not break the system fish shell
 # with the syntax of the config files made for the newer fish versions
 
-if set --query FISH_LOCAL_UPGRADE
+if set --query __FISH_BREAK_RECURSION
     exit # break infinite recursion
 end
 
 set --export FISH_LOCAL_UPGRADE 1
-
-if not status --is-interactive
-    exit
-end
-
+set __FISH_BREAK_RECURSION 1
 set fish_path (readlink /proc/(echo %self)/exe)
 
 if ~/.local/bin/fish_version 4 0 0 $fish_path
     exit # current fish version is good enough
 end
 
-exec ~/.local/bin/fish
+set args (cat /proc/(echo %self)/cmdline | string split0)
+set args[1] ~/.local/bin/fish
+
+exec $args
