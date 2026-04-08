509c9859074cb98410e58c6dbefbb5d01b9e4f21
Iliya Iliev <iliq0000@proton.me>
2026-04-08 15:07:45 +0300

    fish: add brew installed completions

--- private_dot_config/fish/conf.d/15_homebrew.fish
+++ private_dot_config/fish/conf.d/15_homebrew.fish
@@ -1,3 +1,7 @@
-if test -x /home/linuxbrew/.linuxbrew/bin/brew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)
+if not test -x /home/linuxbrew/.linuxbrew/bin/brew
+    exit
 end
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)
+set --export --append --path fish_complete_path \
+    /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/
