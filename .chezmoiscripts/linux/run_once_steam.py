8c9849483df6536ba209b1b7343af8051a0bf4c3
Iliya Iliev <iliq0000@proton.me>
2026-04-10 11:05:59 +0300

    steam: tweak script

--- .chezmoiscripts/linux/run_once_steam.py
+++ .chezmoiscripts/linux/run_once_steam.py
@@ -14,29 +14,26 @@ KCD2 = "1771300"
 
 localconfigs: set[Path] = set()
 
-roots = [
-    Path.home() / ".local/share/Steam",  #                        # native
-    Path.home() / ".var/app/com.valvesoftware.Steam/data/Steam",  # flatpak
-]
-
-for root in roots:
-    localconfigs.update(root.glob("userdata/*/config/localconfig.vdf"))
-
+for steam_root in (
+    Path("~/.local/share/Steam").expanduser(),
+    Path("~/.var/app/com.valvesoftware.Steam/data/Steam").expanduser(),
+):
+    localconfigs.update(steam_root.glob("userdata/*/config/localconfig.vdf"))
 
 for path in localconfigs:
+    with open(path) as localconfig:
+        parsed_vdf = parse(localconfig)
+
     id3 = path.parent.parent.name
-
-    with open(path) as local_config:
-        parsed_vdf = parse(local_config)
-
     system = parsed_vdf["UserLocalConfigStore"]["system"]
+    apps = parsed_vdf["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["apps"]
+
     system["InGameOverlayShortcutKey"] = "Ctrl\tShift\tKEY_TAB"
 
-    apps = parsed_vdf["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["apps"]
     language = "-language 'Bulgarian'" if id3 == PERSONAL_ID3 else ""
-
     apps[CS2]["LaunchOptions"] = f"gamemoderun %command% {language}".strip()
+
     apps[KCD2]["LaunchOptions"] = "gamemoderun %command%"
 
-    with open(path, "w") as local_config:
-        dump(parsed_vdf, local_config, pretty=True)
+    with open(path, "w") as localconfig:
+        dump(parsed_vdf, localconfig, pretty=True)
