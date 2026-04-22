2d7b314b07cdfcbd4870c887cc97d59f97649554
Iliya Iliev <iliq0000@proton.me>
2026-04-22 16:34:04 +0300

    powershell: fix wrong variable in chezmoi temp profile

--- .chezmoi/chezmoi.ps1
+++ .chezmoi/chezmoi.ps1
@@ -1,7 +1,7 @@
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
     + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-$LOCAL_BIN = "$env:Path\.local\bin"
+$LOCAL_BIN = "$env:USERPROFILE\.local\bin"
 
 if ($env:Path -notlike "*$LOCAL_BIN*") {
     $env:Path += ";$LOCAL_BIN"
