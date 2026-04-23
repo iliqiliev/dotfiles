8b28cb5c04a63459329a9364993301be49fc01cd
Iliya Iliev <iliq0000@proton.me>
2026-04-23 15:46:44 +0300

    pwsh: set aliases

--- scoop/persist/pwsh/profile.ps1
+++ scoop/persist/pwsh/profile.ps1
@@ -1,6 +1,24 @@
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
+# Aliases start
+Set-Alias 'ccopy' Set-Clipboard
+Set-Alias 'cpaste' Get-Clipboard
+
+Set-Alias 'cz' 'chezmoi'
+Set-Alias 'ff' 'fastfetch'
+
+if (Get-Command bat -ErrorAction SilentlyContinue) {
+    Set-Alias 'cat' 'bat'
+}
+
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+    Set-Alias 'ls' 'eza'
+    function ll {eza -l @args}
+    function la {eza -la @args}
+    function tree {eza -T @args}
+}
+# Aliases end
 
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     Invoke-Expression (&starship init powershell)
