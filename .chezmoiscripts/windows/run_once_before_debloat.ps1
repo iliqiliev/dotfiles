9d8bc7f338772ebb03a8662751ebd010e8a3a977
Iliya Iliev <iliq0000@proton.me>
2026-04-24 16:08:00 +0300

    scripts: windows: add debloat script

---
+++ .chezmoiscripts/windows/run_once_before_debloat.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = 'Stop'
+
+
+Write-Host 'Debloating...' -ForegroundColor Blue
+
+$removed = 0
+$PACKAGES = @(
+    'Microsoft.WidgetsPlatformRuntime'
+    'MicrosoftWindows.Client.WebExperience'
+)
+
+foreach ($package_name in $PACKAGES) {
+    Write-Host "Removing '$package_name' ..." -ForegroundColor Blue
+    $package = Get-AppxPackage $package_name
+
+    if (!$package) {
+        Write-Host "'$package_name' not found. Continuing." -ForegroundColor Green
+        continue
+    }
+
+    Remove-AppxPackage $package
+    Write-Host "'$package_name' successfully removed." -ForegroundColor Green
+    $removed++
+}
+
+if ($removed -eq 0) {
+    Write-Host "No packages to debloat. :)" -ForegroundColor Green
+    exit 0
+}
+
+Write-Host "$removed packages debloated." -ForegroundColor Green
+
+Restart-Explorer
