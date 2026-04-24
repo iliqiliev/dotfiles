f77ae784bbc6e90050dd5cb6087c93f6bd21c101
Iliya Iliev <iliq0000@proton.me>
2026-04-24 20:19:22 +0300

    install.ps1: update to match shell script

--- install.ps1
+++ install.ps1
@@ -4,5 +4,10 @@ $CHEZMOI_URL = 'https://get.chezmoi.io/ps1'
 $CHEZMOI_DIR = '~/.local/bin'
 $CHEZMOI = "$CHEZMOI_DIR/chezmoi"
 
+
+Write-Host "Installing chezmoi to '$CHEZMOI'" -ForegroundColor Blue
 Invoke-Expression "&{$(Invoke-RestMethod $CHEZMOI_URL)} -BinDir $CHEZMOI_DIR"
-& $CHEZMOI init iliqiliev --apply --depth 1
+
+$CHEZMOI_ARGS = @('init', 'iliqiliev', '--apply', '--depth', '1')
+Write-Host "Running 'chezmoi $CHEZMOI_ARGS' ..." -ForegroundColor Blue
+& $CHEZMOI @CHEZMOI_ARGS
