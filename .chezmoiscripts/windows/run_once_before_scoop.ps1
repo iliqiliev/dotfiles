aba9b74eb030c624334167dd80c10a3a62238b4d
Iliya Iliev <iliq0000@proton.me>
2026-04-30 20:12:42 +0300

    scoop: make script more readable

--- .chezmoiscripts/windows/run_once_before_scoop.ps1
+++ .chezmoiscripts/windows/run_once_before_scoop.ps1
@@ -1,13 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 
-$BUCKETS = @(
-    "extras"
-    "sysinternals"
-    "nerd-fonts"
-    "iliya https://github.com/iliqiliev/iliya-bucket"
-)
-
 if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
     Write-Host "Scoop not found. Installing it..." -ForegroundColor Blue
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
@@ -21,10 +14,17 @@ if (!(Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Host "Git installed successfully." -ForegroundColor Green
 }
 
+$BUCKETS = @(
+    "extras"
+    "sysinternals"
+    "nerd-fonts"
+    "iliya https://github.com/iliqiliev/iliya-bucket"
+)
+
 foreach ($bucket in $BUCKETS) {
     $name, $url = $bucket.Split(" ", 2)
 
-    if (Test-Path "$env:USERPROFILE\scoop\buckets\$name" -PathType Container) {
+    if (Test-Path "~/scoop/buckets/$name" -PathType Container) {
         Write-Host "The $name bucket is added already." -ForegroundColor Green
         continue
     }
@@ -34,12 +34,13 @@ foreach ($bucket in $BUCKETS) {
 
 Write-Host "Scoop buckets added successfully." -ForegroundColor Green
 
+
 Write-Host "Installing scoop packages..." -ForegroundColor Blue
 
-$SCOOP_PACKAGES = (Get-ChildItem ~/scoop/buckets/*/bucket/*.json).BaseName
-$PACKAGES = $env:PACKAGES -split " "
+$ALL_SCOOP_PACKAGES = (Get-ChildItem "~/scoop/buckets/*/bucket/*.json").BaseName
+$REQUESTED_PACKAGES = $env:PACKAGES -split " "
 $AVAILABLE_PACKAGES = (
-    Compare-Object $SCOOP_PACKAGES $PACKAGES -PassThru -IncludeEqual -ExcludeDifferent
+    Compare-Object $ALL_SCOOP_PACKAGES $REQUESTED_PACKAGES -PassThru -IncludeEqual -ExcludeDifferent
 )
 
 scoop install --no-update-scoop @AVAILABLE_PACKAGES
