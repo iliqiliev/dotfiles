1bf3d59f3d534b16cd1add4e7164bb0af438482a
Iliya Iliev <iliq0000@proton.me>
2026-04-30 14:17:15 +0300

    zen: add more settings to user.js

--- private_dot_config/zen/profile/user.js
+++ private_dot_config/zen/profile/user.js
@@ -1,2 +1,6 @@
-/* Skip Zen's welcome screen */
+/* Show a warning when opening about:config. */
+user_pref("browser.aboutConfig.showWarning", false);
+/* Enable auto-scrolling with middle mouse button. */
+user_pref("general.autoScroll", true);
+/* Skip Zen's welcome screen. */
 user_pref("zen.welcome-screen.seen", true);
