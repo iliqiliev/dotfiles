1688a9da0263799793719c9c056719500128d4df
Iliya Iliev <iliq0000@proton.me>
2026-04-30 14:22:37 +0300

    zen: user.js: Allow selecting a different default search engine in private mode.

--- private_dot_config/zen/profile/user.js
+++ private_dot_config/zen/profile/user.js
@@ -1,5 +1,7 @@
 /* Show a warning when opening about:config. */
 user_pref("browser.aboutConfig.showWarning", false);
+/* Allow selecting a different default search engine in private mode. */
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
 /* Enable auto-scrolling with middle mouse button. */
 user_pref("general.autoScroll", true);
 /* Skip Zen's welcome screen. */
