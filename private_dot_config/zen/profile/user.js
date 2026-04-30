f78fb27744fb153ee5a0471ae7c14bb856114e74
Iliya Iliev <iliq0000@proton.me>
2026-04-30 14:32:05 +0300

    zen: dont ask to remember logins and credit cards

--- private_dot_config/zen/profile/user.js
+++ private_dot_config/zen/profile/user.js
@@ -2,7 +2,11 @@
 user_pref("browser.aboutConfig.showWarning", false);
 /* Allow selecting a different default search engine in private mode. */
 user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+/* Do not ask to save credit card details. */
+user_pref("extensions.formautofill.creditCards.enabled", false);
 /* Enable auto-scrolling with middle mouse button. */
 user_pref("general.autoScroll", true);
+/* Do not ask to save login details. */
+user_pref("signon.rememberSignons", false);
 /* Skip Zen's welcome screen. */
 user_pref("zen.welcome-screen.seen", true);
