8745a942a783014fafde041092f6d9104fbac2ba
Iliya Iliev <iliq0000@proton.me>
2026-05-04 16:03:01 +0300

    powershell: add Set-RegistryHKCU function

--- .chezmoi/chezmoi.ps1
+++ .chezmoi/chezmoi.ps1
@@ -5,6 +5,39 @@ function Restart-Explorer {
     Write-Host "'explorer.exe' restarted successfully." -ForegroundColor Green
 }
 
+function Set-RegistryHKCU {
+    param (
+        [Parameter(Mandatory)]
+        [string]$PathName,
+                $Value
+    )
+
+    $PathName = "HKCU:\$($PathName)"
+
+    if ($Value -eq $null) {
+        if (Test-Path $PathName) {
+            Return 0
+        }
+
+        New-Item -Confirm -Force -Path $PathName > $null
+        Return 0
+    }
+
+    $Path = Split-Path $PathName -Parent
+    $Name = Split-Path $PathName -Leaf
+    $TypeParam = @{}
+
+    if ($Value -match '%\S+%') {
+        $TypeParam.Type = 'ExpandString'
+    }
+
+    if (-not (Test-Path $Path)) {
+        New-Item -Confirm -Force -Path $Path > $null
+    }
+
+    Set-ItemProperty -Confirm -Path $Path -Name $Name -Value $Value @TypeParam
+}
+
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
     + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
