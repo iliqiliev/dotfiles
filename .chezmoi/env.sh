f0a8858a7cc4409a07735a39695de2bae295a369
Iliya Iliev <iliq0000@proton.me>
2026-05-05 14:26:43 +0300

    chezmoi: small tweaks

--- .chezmoi/env.sh
+++ .chezmoi/env.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
-ANSI_RESET='\033[0m'
+ANSI_RESET='\033[m'
+ANSI_BRIGHT_RED='\033[91m'
+ANSI_BRIGHT_GREEN='\033[92m'
+ANSI_BRIGHT_YELLOW='\033[93m'
+ANSI_BRIGHT_BLUE='\033[94m'
 
 print_red() {
-    ANSI_BRIGHT_RED='\033[91m'
     printf "${ANSI_BRIGHT_RED}%b\n${ANSI_RESET}" "$*"
 }
 
 print_green() {
-    ANSI_BRIGHT_GREEN='\033[92m'
     printf "${ANSI_BRIGHT_GREEN}%b\n${ANSI_RESET}" "$*"
 }
 
 print_yellow() {
-    ANSI_BRIGHT_YELLOW='\033[93m'
     printf "${ANSI_BRIGHT_YELLOW}%b\n${ANSI_RESET}" "$*"
 }
 
 print_blue() {
-    ANSI_BRIGHT_BLUE='\033[94m'
     printf "${ANSI_BRIGHT_BLUE}%b\n${ANSI_RESET}" "$*"
 }
