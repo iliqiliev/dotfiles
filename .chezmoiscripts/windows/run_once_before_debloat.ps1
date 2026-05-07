5bbbce81e91d49368eb9efbaf29e7946db152fa3
Iliya Iliev <iliq0000@proton.me>
2026-05-07 14:07:08 +0300

    windows: add option for deep debloat
    also add more bloat packages

--- .chezmoiscripts/windows/run_once_before_debloat.ps1
+++ .chezmoiscripts/windows/run_once_before_debloat.ps1
@@ -1,22 +1,46 @@
 $ErrorActionPreference = 'Stop'
 
 
-Write-Host 'Removing bloat packages...' -ForegroundColor Blue
-
-$removed = 0
 $PACKAGES = @(
+    'Clipchamp.Clipchamp'
     'Microsoft.BingNews'
     'Microsoft.BingSearch'
     'Microsoft.BingWeather'
     'Microsoft.GetHelp'
     'Microsoft.MicrosoftSolitaireCollection'
+    'Microsoft.MicrosoftStickyNotes'
+    'Microsoft.PowerAutomateDesktop'
     'Microsoft.StartExperiencesApp'
     'Microsoft.Todos'
     'Microsoft.WidgetsPlatformRuntime'
+    'Microsoft.Windows.DevHome'
+    'Microsoft.WindowsAlarms'
     'Microsoft.WindowsFeedbackHub'
+    'Microsoft.WindowsSoundRecorder'
+    'Microsoft.ZuneMusic'
+    'MicrosoftCorporationII.QuickAssist'
     'MicrosoftWindows.Client.WebExperience'
 )
 
+$ADDITIONAL_PACKAGES = @(
+#   'Microsoft.MicrosoftEdge.Stable'
+    "Microsoft.Paint"
+    "Microsoft.OutlookForWindows"
+    "MSTeams"
+)
+
+$deep_debloat = $host.UI.PromptForChoice(
+    "Perform deep debloat?",
+    "This will remove packages like Edge, OneDrive, Teams, etc.",
+    @("Yes", "No"),
+    1
+) -eq 0
+
+if ($deep_debloat) { $PACKAGES += $ADDITIONAL_PACKAGES }
+
+Write-Host 'Removing bloat packages...' -ForegroundColor Blue
+
+$removed = 0
 foreach ($package_name in $PACKAGES) {
     Write-Host "Removing '$package_name' ... " -ForegroundColor Blue -NoNewline
     $package = Get-AppxPackage $package_name
