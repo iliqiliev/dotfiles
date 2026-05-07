e7079e0f94bd894462172d60c5deb2836d858d27
Iliya Iliev <iliq0000@proton.me>
2026-05-07 23:33:07 +0300

    fish: format configs

--- private_dot_config/fish/conf.d/20_aliases.fish
+++ private_dot_config/fish/conf.d/20_aliases.fish
@@ -1,19 +1,15 @@
-alias ccopy  "fish_clipboard_copy"
-alias cpaste "fish_clipboard_paste"
+alias ccopy fish_clipboard_copy
+alias cpaste fish_clipboard_paste
 
 if command --query bat
-    alias cat "bat"
+    alias cat bat
 end
 
 if command --query eza
-    alias ls "eza"
+    alias ls eza
     alias tree "eza -T"
 end
 
 if command --query fdfind
-    alias fd "fdfind"
-end
-
-if command --query ip
-    alias ip "ip --color"
+    alias fd fdfind
 end
--- private_dot_config/fish/conf.d/30_abbreviations.fish
+++ private_dot_config/fish/conf.d/30_abbreviations.fish
@@ -1,6 +1,6 @@
-abbr cz "chezmoi"
-abbr ff "fastfetch"
-abbr fp "flatpak"
-abbr sc  "systemctl"
+abbr cz chezmoi
+abbr ff fastfetch
+abbr fp flatpak
+abbr sc systemctl
 abbr scu "systemctl --user"
 abbr ssc "sudo systemctl"
