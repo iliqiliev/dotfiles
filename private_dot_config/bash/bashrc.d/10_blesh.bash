0488cfa5d72a14b50d3c31be999ed567314ad188
Iliya Iliev <iliq0000@proton.me>
2026-05-07 23:27:17 +0300

    bash: update configs

--- private_dot_config/bash/bashrc.d/10_blesh.bash
+++ private_dot_config/bash/bashrc.d/10_blesh.bash
@@ -1,4 +1,13 @@
-if test -r ~/.local/share/blesh/ble.sh; then
-    # shellcheck source=/dev/null
-    source ~/.local/share/blesh/ble.sh
+if test ! -r ~/.local/share/blesh/ble.sh; then
+    return
 fi
+
+# shellcheck source=/dev/null
+source ~/.local/share/blesh/ble.sh
+
+ble-sabbrev cz="chezmoi"
+ble-sabbrev ff="fastfetch"
+ble-sabbrev fp="flatpak"
+ble-sabbrev sc= "systemctl"
+ble-sabbrev scu="systemctl --user"
+ble-sabbrev ssc="sudo systemctl"
--- private_dot_config/bash/bashrc.d/aliases.bash
+++ private_dot_config/bash/bashrc.d/aliases.bash
@@ -1,19 +1,11 @@
-alias ll="ls -l"
-alias la="ls -la"
-
 if command -v bat >/dev/null; then
     alias cat="bat"
 fi
 
-if command -v chezmoi >/dev/null; then
-    alias cz="chezmoi"
-fi
+alias ll="ls -l"
+alias la="ls -la"
 
 if command -v eza >/dev/null; then
     alias ls="eza"
     alias tree="eza -T"
-fi
-
-if command -v fastfetch >/dev/null; then
-    alias ff="fastfetch"
 fi
