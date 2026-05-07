0e009d7f548e8eac4b74cb66c2b1e3d3d1eff697
Iliya Iliev <iliq0000@proton.me>
2026-05-07 23:28:09 +0300

    fix typo

--- private_dot_config/bash/bashrc.d/10_blesh.bash
+++ private_dot_config/bash/bashrc.d/10_blesh.bash
@@ -8,6 +8,6 @@ source ~/.local/share/blesh/ble.sh
 ble-sabbrev cz="chezmoi"
 ble-sabbrev ff="fastfetch"
 ble-sabbrev fp="flatpak"
-ble-sabbrev sc= "systemctl"
+ble-sabbrev sc="systemctl"
 ble-sabbrev scu="systemctl --user"
 ble-sabbrev ssc="sudo systemctl"
