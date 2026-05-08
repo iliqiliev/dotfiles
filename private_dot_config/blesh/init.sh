c01c7d702a928fda9f7bc98b47d35f9bab9cfe3c
Iliya Iliev <iliq0000@proton.me>
2026-05-08 11:32:18 +0300

    blesh: always expand abbreviations

--- private_dot_config/blesh/init.sh
+++ private_dot_config/blesh/init.sh
@@ -1,6 +1,10 @@
+# Set abbreviations
 ble-sabbrev cz="chezmoi"
 ble-sabbrev ff="fastfetch"
 ble-sabbrev fp="flatpak"
 ble-sabbrev sc="systemctl"
 ble-sabbrev scu="systemctl --user"
 ble-sabbrev ssc="sudo systemctl"
+
+# Expand abbreviations if they are the only input
+bleopt edit_magic_accept=sabbrev:verify-syntax
