1a3523a0f62040b1669414593d38465653b05e46
Iliya Iliev <iliq0000@proton.me>
2026-05-11 17:35:15 +0300

    nvim: use 4 spaces for tabs

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,1 +1,7 @@
+-- Initialize lazy.nvim
 require("config.lazy")
+
+-- Formatting
+vim.opt.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>.
+vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent.
+vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for.
