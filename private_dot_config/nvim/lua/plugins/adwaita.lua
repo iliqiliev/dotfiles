7c2f6afc08b0d24d0a4b082f4ce7481a16662938
Iliya Iliev <iliq0000@proton.me>
2026-05-12 13:42:21 +0300

    nvim: tweak advaita plugin

--- private_dot_config/nvim/lua/plugins/adwaita.lua
+++ private_dot_config/nvim/lua/plugins/adwaita.lua
@@ -3,9 +3,8 @@ return {
         "Mofiqul/adwaita.nvim",
         lazy = false,
         priority = 1000,
-        -- configure and set on startup
         config = function()
-            vim.g.adwaita_transparent = true
+            vim.g.adwaita_transparent = true -- makes the background transparent
             vim.cmd('colorscheme adwaita')
         end
     }
