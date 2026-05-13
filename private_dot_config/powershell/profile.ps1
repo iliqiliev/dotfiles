d26c4ec3942bfcd3460fff004ab6f2847cd7e73c
Iliya Iliev <iliq0000@proton.me>
2026-05-13 14:37:28 +0300

    powershell: set HistorySearchCursorMovesToEnd

--- private_dot_config/powershell/profile.ps1
+++ private_dot_config/powershell/profile.ps1
@@ -1,8 +1,10 @@
 Set-PSReadLineKeyHandler Ctrl+c    RevertLine
 Set-PSReadlineKeyHandler Ctrl+d    DeleteCharOrExit
 Set-PSReadLineKeyHandler Tab       MenuComplete
+
 Set-PSReadlineKeyHandler UpArrow   HistorySearchBackward
 Set-PSReadlineKeyHandler DownArrow HistorySearchForward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
 Set-Alias 'ccopy' Set-Clipboard
 Set-Alias 'cpaste' Get-Clipboard
