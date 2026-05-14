e414db61211bb1ac5bf02a310f687d74237b560c
Iliya Iliev <iliq0000@proton.me>
2026-05-14 17:53:21 +0300

    lazy.nvim: tweak initialization

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,8 @@
+-- Setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- Initialize lazy.nvim
 require("config.lazy")
 
