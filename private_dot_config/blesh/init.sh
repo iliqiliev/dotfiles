32fbd025bc5975af00c27ce64d015dc2bfec32d6
Iliya Iliev <iliq0000@proton.me>
2026-05-14 15:30:09 +0300

    blesh: Disable highlighting based on filenames.

--- private_dot_config/blesh/init.sh
+++ private_dot_config/blesh/init.sh
@@ -1,4 +1,5 @@
-# Set abbreviations
+# Set abbreviations.
+# See https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#user-content-sec-sabbrev
 ble-sabbrev cz="chezmoi"
 ble-sabbrev ff="fastfetch"
 ble-sabbrev fp="flatpak"
@@ -6,5 +7,8 @@ ble-sabbrev sc="systemctl"
 ble-sabbrev scu="systemctl --user"
 ble-sabbrev ssc="sudo systemctl"
 
-# Expand abbreviations if they are the only input
+# Expand abbreviations if they are the only input.
 bleopt edit_magic_accept=sabbrev:verify-syntax
+
+# Disable highlighting based on filenames.
+bleopt highlight_filename=
