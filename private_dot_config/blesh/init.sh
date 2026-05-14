15eade4f341d3d70f682d9ad216988bcd639968b
Iliya Iliev <iliq0000@proton.me>
2026-05-14 22:14:36 +0300

    ble.sh: source fish abbreviations directly to avoid duplication

--- private_dot_config/blesh/init.sh
+++ private_dot_config/blesh/init.sh
@@ -1,11 +1,14 @@
+# Mimic behaviour of fish so that existing abbreviations can be used.
+function abbr() {
+    local KEY="$1"
+    local VALUE="$2"
+    # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#user-content-sec-sabbrev
+    ble-sabbrev "$KEY"="$VALUE"
+}
+
 # Set abbreviations.
-# See https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#user-content-sec-sabbrev
-ble-sabbrev cz="chezmoi"
-ble-sabbrev ff="fastfetch"
-ble-sabbrev fp="flatpak"
-ble-sabbrev sc="systemctl"
-ble-sabbrev scu="systemctl --user"
-ble-sabbrev ssc="sudo systemctl"
+# shellcheck source=private_dot_config/fish/conf.d/30_abbreviations.fish
+source ~/.config/fish/conf.d/30_abbreviations.fish
 
 # Expand abbreviations if they are the only input.
 bleopt edit_magic_accept=sabbrev:verify-syntax
