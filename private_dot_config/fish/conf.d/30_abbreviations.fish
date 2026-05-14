3e782450d2097805bdc1cd51aea283c3a7ad4565
Iliya Iliev <iliq0000@proton.me>
2026-05-14 22:15:54 +0300

    add more abbreviations

--- private_dot_config/fish/conf.d/30_abbreviations.fish
+++ private_dot_config/fish/conf.d/30_abbreviations.fish
@@ -1,6 +1,8 @@
 abbr cz chezmoi
+abbr czu "chezmoi update --init"
 abbr ff fastfetch
 abbr fp flatpak
 abbr sc systemctl
 abbr scu "systemctl --user"
 abbr ssc "sudo systemctl"
+abbr sscr "sudo systemctl restart"
