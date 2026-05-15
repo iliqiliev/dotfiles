4d16efdd7bd35c828003a7d13471f3da09076fae
Iliya Iliev <iliq0000@proton.me>
2026-05-15 10:39:09 +0300

    nvim: format config with stylua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -8,5 +8,5 @@ require("config.lazy")
 
 -- Formatting
 vim.opt.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>.
-vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent.
-vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
--- private_dot_config/nvim/lua/plugins/shellcheck.lua
+++ private_dot_config/nvim/lua/plugins/shellcheck.lua
@@ -1,10 +1,10 @@
 return {
-    {
-        'pablos123/shellcheck.nvim',
-        config = function()
-            require 'shellcheck-nvim'.setup {
-                shellcheck_options = { '-x', '--enable=all', },
-            }
-        end
-    }
+   {
+      "pablos123/shellcheck.nvim",
+      config = function()
+         require("shellcheck-nvim").setup({
+            shellcheck_options = { "-x", "--enable=all" },
+         })
+      end,
+   },
 }
