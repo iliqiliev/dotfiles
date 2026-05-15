a7833be6670a850b063c774e23de857fa170307f
Iliya Iliev <iliq0000@proton.me>
2026-05-15 15:36:37 +0300

    install/sh: use https, drop openssl downloader

--- helpers/install.sh
+++ helpers/install.sh
@@ -5,7 +5,7 @@ set -eu # exit on [e]rror and [u]nset variables
 if ! CHEZMOI="$(command -v chezmoi)"; then
     CHEZMOI="${HOME}/.local/bin/chezmoi"
     CHEZMOI_DIR="$(dirname "$CHEZMOI")"
-    CHEZMOI_URL="get.chezmoi.io"
+    CHEZMOI_URL="https://get.chezmoi.io/"
 
     printf "\033[94m%b\n\033[m" "Downloading chezmoi to '${CHEZMOI_DIR}' ..."
 
@@ -13,8 +13,6 @@ if ! CHEZMOI="$(command -v chezmoi)"; then
         CHEZMOI_INSTALLER="$(curl -LSfs $CHEZMOI_URL)"
     elif command -v wget >/dev/null; then
         CHEZMOI_INSTALLER="$(wget -qO- $CHEZMOI_URL)"
-    elif command -v openssl >/dev/null; then
-        CHEZMOI_INSTALLER="$(printf 'GET / HTTP/1.0\nHost:%s\n\n' $CHEZMOI_URL | openssl s_client -quiet $CHEZMOI_URL:443 2>/dev/null)"
     else
         printf "\033[91m%b\n\033[m" "To install chezmoi, you must have curl or wget installed."
         exit 1
