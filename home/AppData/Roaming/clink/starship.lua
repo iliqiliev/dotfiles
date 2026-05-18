521b567114f6a1e37ab2ede01c7062858638f723
Iliya Iliev <iliq0000@proton.me>
2026-05-18 17:46:08 +0300

    clink: set starship prompt

---
+++ home/AppData/Roaming/clink/starship.lua
@@ -0,0 +1,54 @@
+---@diagnostic disable
+
+local starship_prompt = clink.promptfilter(5)
+
+start_time = os.clock()
+end_time = 0
+curr_duration = 0
+is_line_empty = true
+
+clink.onbeginedit(function()
+   end_time = os.clock()
+   if not is_line_empty then
+      curr_duration = end_time - start_time
+   end
+end)
+
+clink.onendedit(function(curr_line)
+   if starship_precmd_user_func ~= nil then
+      starship_precmd_user_func(curr_line)
+   end
+   start_time = os.clock()
+   if string.len(string.gsub(curr_line, "^%s*(.-)%s*$", "%1")) == 0 then
+      is_line_empty = true
+   else
+      is_line_empty = false
+   end
+end)
+
+function starship_prompt:filter(prompt)
+   return io.popenyield(
+      os.getenv("USERPROFILE")
+         .. "/scoop/apps/starship/current/starship.exe"
+         .. " prompt"
+         .. " --status="
+         .. os.geterrorlevel()
+         .. " --cmd-duration="
+         .. math.floor(curr_duration * 1000)
+         .. " --terminal-width="
+         .. console.getwidth()
+         .. " --keymap="
+         .. rl.getvariable("keymap")
+   ):read("*a")
+end
+
+local characterset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+local randomkey = ""
+math.randomseed(os.time())
+for i = 1, 16 do
+   local rand = math.random(#characterset)
+   randomkey = randomkey .. string.sub(characterset, rand, rand)
+end
+
+os.setenv("STARSHIP_SHELL", "cmd")
+os.setenv("STARSHIP_SESSION_KEY", randomkey)
