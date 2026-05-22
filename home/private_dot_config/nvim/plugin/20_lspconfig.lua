da6396fa24c778f0b08866ff587fe46cf0f7624e
Iliya Iliev <iliq0000@proton.me>
2026-05-22 21:43:38 +0300

    nvim: install lsp, formatters, parsers automatically using ensure

--- home/private_dot_config/nvim/plugin/20_lspconfig.lua
+++ home/private_dot_config/nvim/plugin/20_lspconfig.lua
@@ -1,1 +1,10 @@
 vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
+
+vim.lsp.enable({
+   "basedpyright", -- Python.
+   "bashls", -- Bash/sh.
+   "fish_lsp", -- Fish.
+   "lua_ls", -- Lua.
+   "tclsp", -- TCL.
+   "tombi", -- TOML.
+})
---
+++ home/private_dot_config/nvim/plugin/30_ensure.lua
@@ -0,0 +1,4 @@
+vim.pack.add({ "https://github.com/noirbizarre/ensure.nvim" })
+require("ensure").setup({
+   install = true, -- Install tools on startup.
+})
