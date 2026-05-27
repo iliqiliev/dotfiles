3f287977b20100b38150b5ed8e54af4182291b99
Iliya Iliev <iliq0000@proton.me>
2026-05-27 14:02:37 +0300

    nvim: use more generic config structure

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,15 +1,13 @@
 -- Variables.
-local globals = vim.g
-globals.mapleader = " " -- This is used as the value of the special string <Leader>.
-globals.maplocalleader = "\\" -- Like <Leader> but for mappings which are local to a buffer.
-globals["chezmoi#use_tmp_buffer"] = true -- For better chezmoi filetype detection.
+vim.g.mapleader = " " -- This is used as the value of the special string <Leader>.
+vim.g.maplocalleader = "\\" -- Like <Leader> but for mappings which are local to a buffer.
+vim.g["chezmoi#use_tmp_buffer"] = true -- For better chezmoi filetype detection.
 
 -- Options.
-local options = vim.opt
-options.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>.
-options.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
-options.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
-options.wildmode = "longest:full,full" -- Completion mode used for the 'wildchar'.
+vim.opt.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>.
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.wildmode = "longest:full,full" -- Completion mode used for the 'wildchar'.
 
 -- Plugins.
 vim.pack.add({
