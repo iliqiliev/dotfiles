f6a5e9bae5e4130b25c10222620dc45f48d1e55b
Iliya Iliev <iliq0000@proton.me>
2026-05-28 17:35:03 +0300

    nvim: show better error message if version is too old for vim.pack

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -10,6 +10,14 @@ vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
 vim.opt.wildmode = "longest:full,full" -- Completion mode used for the 'wildchar'.
 
 -- Plugins.
+if not vim.pack then
+   vim.notify(
+      "Neovim version is older than 0.12, plugins will not be installed.",
+      vim.log.levels.WARN
+   )
+   return
+end
+
 vim.pack.add({
    "https://github.com/alker0/chezmoi.vim",
    "https://github.com/mason-org/mason.nvim",
