216d30b50df93b35c513a8b87516abae9450dc89
Iliya Iliev <iliq0000@proton.me>
2026-06-01 14:38:20 +0300

    fish: tweak comments in config

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,20 +1,20 @@
-# conf.d is loaded first
+# './conf.d' is loaded first.
 
-# Upgrade fish version < 2.3 (does not load conf.d)
+# Fish version < 2.3 does not load './conf.d'.
 if not set --query FISH_LOCAL_UPGRADE
     source ~/.config/fish/conf.d/00_upgrade.fish
 end
 
-# Disabling fish_greeting
+# Disabling fish_greeting.
 set --export fish_greeting
 
-# Add directories to PATH
+# Add directories to PATH.
 fish_add_path \
     $HOME/.local/bin \
     $HOME/.local/share/mise/shims \
     /var/lib/flatpak/exports/bin
 
-# Set EDITOR and VIMRUNTIME if nvim is available
+# Set EDITOR and VIMRUNTIME if neovim is installed.
 if command --query nvim
     set --export EDITOR nvim
     set --export LESS "-FMRXix4 --mouse --use-color --wheel-lines=3"
@@ -23,9 +23,9 @@ if command --query nvim
     set --export VIMRUNTIME (nvim --clean --headless --cmd 'echo $VIMRUNTIME|q' 2>&1)
 end
 
-# Source configs for interactive sessions
+# Source configs for interactive sessions.
 if status is-interactive
-    # Silence warnings about unknown keys in config file on old starship versions
+    # Ignore warnings: old starship versions warn about unknown keys in config file.
     set --export STARSHIP_LOG error
     command --query starship; and starship init fish | source
     command --query zoxide; and zoxide init fish | source
