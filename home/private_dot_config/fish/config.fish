0c91f1607682b5f7a15d0fbcde1a438f1b5aedb5
Iliya Iliev <iliq0000@proton.me>
2026-06-08 10:43:19 +0300

    mise: do not install usage-cli

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,20 +1,22 @@
-# './conf.d' is loaded first.
+# `./conf.d` is loaded first.
 
-# Fish version < 2.3 does not load './conf.d'.
+# Fish version < 2.3 does not load `./conf.d`.
 if not set --query FISH_LOCAL_UPGRADE
     source ~/.config/fish/conf.d/00_upgrade.fish
 end
 
 # Disabling fish_greeting.
 set --export fish_greeting
+# Disable automatic activation of `mise` vendor_conf.d script.
+set --export MISE_FISH_AUTO_ACTIVATE 0
 
-# Add directories to PATH.
+# Add directories to `PATH`.
 fish_add_path \
     $HOME/.local/bin \
     $HOME/.local/share/mise/shims \
     /var/lib/flatpak/exports/bin
 
-# Set EDITOR and VIMRUNTIME if neovim is installed.
+# Set `EDITOR` and `VIMRUNTIME` if neovim is installed.
 if command --query nvim
     set --export EDITOR nvim
     set --export LESS "-FMRXix4 --mouse --use-color --wheel-lines=3"
@@ -25,7 +27,7 @@ end
 
 # Source configs for interactive sessions.
 if status is-interactive
-    # Ignore warnings: old starship versions warn about unknown keys in config file.
+    # Ignore warnings: old `starship` warns about unknown keys in config file.
     set --export STARSHIP_LOG error
     command --query starship; and starship init fish | source
     command --query zoxide; and zoxide init fish | source
