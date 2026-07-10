94fbf7a0aa43906ec907aa070dc33c3f64c7973e
Iliya Iliev <iliq0000@proton.me>
2026-07-10 13:27:54 +0300

    fish: tweak local upgrade script

--- home/private_dot_config/fish/conf.d/00_upgrade.fish
+++ home/private_dot_config/fish/conf.d/00_upgrade.fish
@@ -1,14 +1,12 @@
-# If the system fish shell version is too old and there one available
-# at ~/.local/bin/fish, exec it as to not break the system fish shell
-# with the syntax of the config files made for the newer fish versions
+# If the system `fish` version is too old and does not support
+# the syntax used in the plugin files
+# then `fish` will break and spam the terminal with error messages.
 
 set --query FISH_LOCAL_UPGRADE; and exit
 set FISH_LOCAL_UPGRADE 1
 
-# if fish version is >= 4.0.0
-if test (echo $FISH_VERSION | awk -F '.' '{print $1}') -ge 4
-    exit
-end
+# $FISH_VERSION >= 4.0.0
+test (echo $FISH_VERSION | awk -F '.' '{print $1}') -ge 4; and exit
 
 set args (cat /proc/(echo %self)/cmdline | tr "\0" "\n" | tail -n +2)
 
