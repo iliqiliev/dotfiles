eff3d627fab637d98a132389ce50d0e608c5a8b0
Iliya Iliev <iliq0000@proton.me>
2026-07-15 21:03:11 +0300

    bash: fallback abbreviations if blesh is not installed

--- home/private_dot_config/blesh/init.sh
+++ home/private_dot_config/blesh/init.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Mimic behaviour of fish so that existing abbreviations can be used.
-function abbr() {
-    local KEY="$1"
-    local VALUE="$2"
-    # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#user-content-sec-sabbrev
-    ble-sabbrev "${KEY}"="${VALUE}"
-}
-
-# Set abbreviations.
-# shellcheck source=home/private_dot_config/fish/conf.d/30_abbreviations.fish
-source ~/.config/fish/conf.d/30_abbreviations.fish
-
 # Expand abbreviations if they are the only input.
 bleopt edit_magic_accept=sabbrev:verify-syntax
 
