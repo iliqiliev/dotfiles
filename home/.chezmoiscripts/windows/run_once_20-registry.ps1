49d1db97034569805f6ca40d455129473fc5b8be
Iliya Iliev <iliq0000@proton.me>
2026-07-16 21:49:08 +0300

    env: drop XDG_BIN_HOME var

--- home/.chezmoiscripts/windows/run_once_20-registry.ps1
+++ home/.chezmoiscripts/windows/run_once_20-registry.ps1
@@ -17,7 +17,6 @@ Set-RegistryHKCU 'Control Panel\Mouse\MouseThreshold2' '0'
 
 # Set environment variables.
 ## XDG Directories.
-Set-RegistryHKCU 'Environment\XDG_BIN_HOME' '%USERPROFILE%\.local\bin'
 Set-RegistryHKCU 'Environment\XDG_CACHE_HOME' '%USERPROFILE%\.cache'
 Set-RegistryHKCU 'Environment\XDG_CONFIG_HOME' '%USERPROFILE%\.config'
 Set-RegistryHKCU 'Environment\XDG_DATA_HOME' '%USERPROFILE%\.local\share'
