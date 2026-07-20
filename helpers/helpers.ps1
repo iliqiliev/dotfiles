5b5aeac31d0efce0bcb3287862a2238d645c8c7a
Iliya Iliev <iliq0000@proton.me>
2026-07-20 20:50:42 +0300

    powershell: drop helper file

--- helpers/helpers.ps1
+++
@@ -1,7 +0,0 @@
-# Refresh PATH.
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
-    + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-if ($args.Count -gt 0) {
-    . $args[0]
-}
