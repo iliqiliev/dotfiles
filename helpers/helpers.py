23671f048e726967a924f772876db2d40af8fe10
Iliya Iliev <iliq0000@proton.me>
2026-07-24 10:24:47 +0300

    ruff: detect python templates as files

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -8,6 +8,6 @@ from rich import print as rich_print
 def restart_explorer() -> None:
     """Restart `explorer.exe`."""
     rich_print("[bright_blue]Restarting 'explorer.exe' ...", end=" ")
-    run(("TASKKILL", "/F", "/IM", "explorer.exe"), stdout=DEVNULL)
+    run(("TASKKILL", "/F", "/IM", "explorer.exe"), check=True, stdout=DEVNULL)
     Popen("explorer.exe")
     rich_print("[bright_green]Done.")
