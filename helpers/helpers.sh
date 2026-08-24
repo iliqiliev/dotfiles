cff5c7509269aeeb82dc2dced60ec8748c06925d
Iliya Iliev <iliq0000@proton.me>
2026-08-24 17:27:22 +0300

    helpers: add -n flag for no new line

--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -6,20 +6,23 @@ export ANSI_BRIGHT_GREEN='\033[92m'
 export ANSI_BRIGHT_YELLOW='\033[93m'
 export ANSI_BRIGHT_BLUE='\033[94m'
 export ANSI_CURSOR_PREVIOUS_LINE='\033[F'
-export ANSI_ERASE_IN_LINE='\033[K'
 
-print_red() {
-    printf "${ANSI_BRIGHT_RED}%b\n${ANSI_RESET}" "$*"
+print_color() {
+    # -n: Do not apped a newline at the end of the message.
+
+    color=$1
+    shift
+
+    end="\n"
+    if test "$1" = "-n"; then
+        end=""
+        shift
+    fi
+
+    printf "${color}%b${end}${ANSI_RESET}" "$*"
 }
 
-print_green() {
-    printf "${ANSI_BRIGHT_GREEN}%b\n${ANSI_RESET}" "$*"
-}
-
-print_yellow() {
-    printf "${ANSI_BRIGHT_YELLOW}%b\n${ANSI_RESET}" "$*"
-}
-
-print_blue() {
-    printf "${ANSI_BRIGHT_BLUE}%b\n${ANSI_RESET}" "$*"
-}
+print_red() { print_color "$ANSI_BRIGHT_RED" "$@"; }
+print_green() { print_color "$ANSI_BRIGHT_GREEN" "$@"; }
+print_yellow() { print_color "$ANSI_BRIGHT_YELLOW" "$@"; }
+print_blue() { print_color "$ANSI_BRIGHT_BLUE" "$@"; }
