f00fca9dc957e1ff738d19f811f4132a6153030f
Iliya Iliev <iliq0000@proton.me>
2026-08-31 12:08:22 +0000

    helpers/py: add helper that checks if a util is busybox

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,11 +1,13 @@
 """Helper module."""
 
 from .helpers import (
+    is_busybox,
     restart_explorer,
     sudo,
 )
 
 __all__ = [
+    "is_busybox",
     "restart_explorer",
     "sudo",
 ]
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,9 +1,24 @@
 """Helper functions."""
 
 from os import environ as env
+from pathlib import Path
+from shutil import which
 from subprocess import DEVNULL, Popen, run
 
 from rich import print as rich_print
+
+
+def is_busybox(utility: str) -> bool:
+    if (utility_path := which(utility)) is None:
+        raise FileNotFoundError(f"'{utility}' is not installed.")
+
+    if (busybox_path := which("busybox")) is None:
+        return False
+
+    utility_path_resolved = Path(utility_path).resolve()
+    busybox_path_resolved = Path(busybox_path).resolve()
+
+    return utility_path_resolved == busybox_path_resolved
 
 
 def restart_explorer() -> None:
